f9261d75c8313ce250ef1ed4339a3fad874163cf
andimaulana18 <andimaulanayusup12@gmail.com>
2024-01-06 11:51:13 +0700

    mini project

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+let popup = document.getElementById("popup");
+
+function openPopup(params) {
+    popup.classList.add("open-popup");
+}
+
+function closePopup(params){
+    popup.classList.remove("open-popup");
+}
